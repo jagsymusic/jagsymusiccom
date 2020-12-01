7778f5f963b3641f23f4911b3e8ad4e6d92063b4
magnuen2k <maen030@student.kristiania.no>
2020-12-02 00:09:36 +0100

    adding songs

--- src/data.js
+++ src/data.js
@@ -11,6 +11,16 @@ function myTracks() {
             name: "All My Love",
             cover: "https://linkstorage.linkfire.com/medialinks/images/9a7d91bc-00cc-4904-b394-9ab940269d1e/artwork-440x440.jpg",
             id: uuidv4()
+        },
+        {
+            name: "Left My Heart In Pain",
+            cover: "https://linkstorage.linkfire.com/medialinks/images/cfd2d124-487a-4149-a6d5-9c91aaabc728/artwork-440x440.jpg",
+            id: uuidv4()
+        },
+        {
+            name: "All My Love",
+            cover: "https://linkstorage.linkfire.com/medialinks/images/9a7d91bc-00cc-4904-b394-9ab940269d1e/artwork-440x440.jpg",
+            id: uuidv4()
         }
     ]
 }
