3e8e6f609f1939ae4c9979f33e925328248625b1
magnuen2k <maen030@student.kristiania.no>
2020-12-14 20:19:04 +0100

    add gallery

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Gallery from "../components/Gallery";
 //components
 import Header from "../components/Header";
 import Music from "../components/Music";
@@ -10,6 +11,7 @@ const HomePage = () => {
             <Header/>
             <Music/>
             <Player/>
+            <Gallery />
         </div>
     )
 }
