2a978a3274821481e5c1708756705f7f39aa0d54
magnuen2k <maen030@student.kristiania.no>
2020-12-18 01:32:07 +0100

    implement footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Socials from "./Socials";
 
 const Footer = () => {
     return (
-        <div>
-            FOOTER HERE
-            
+        <div className="footer-wrapper">
+            <div className="container">
+                <div className="footer">
+                    <div className="footer-copyright">
+                        <p>&copy; 2020 jagsymusic. All rights reserved.</p>
+                    </div>
+                    <div className="footer-some">
+                        <Socials pos="footer-socials" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
