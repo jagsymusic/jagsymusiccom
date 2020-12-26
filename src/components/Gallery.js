f7d5c40d470cd1c29909bd029b5bd3970e3a6a07
Ida Merete Enholm <ida.enholm@me.com>
2020-12-26 17:48:14 +0100

    fix small errors after merge

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,22 +7,20 @@ import instagram from "../img/social/instagram.svg";
 const Gallery = () => {
   const [pics, setPics] = useState(myGallery);
 
-    return (
-        <div className="gallery-wrapper" id="gallery">
-            <div className="gallery container">
-                <h1 className="gallery-title">Gallery</h1>
-                <div className="gallery-container">
-                    {pics.map((img) => (
-                        <Picture img={img.img} name={img.name} key={img.id}/>
-                    ))}
-                </div>
-                <div className="follow-wrapper">
-                    <div className="follow-me-ig">
-                        <img src={instagram}  alt="instagram"/>
-                        <p>Follow me @jagsymusic for more</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="gallery-wrapper" id="gallery">
+      <div className="gallery container">
+        <h1 className="gallery-title">Gallery</h1>
+        <div className="gallery-container">
+          {pics.map((img) => (
+            <Picture img={img.img} name={img.name} key={img.id} />
+          ))}
+        </div>
+        <div className="follow-wrapper">
+          <div className="follow-me-ig">
+            <img src={instagram} alt="instagram" />
+            <p>Follow me @jagsymusic for more</p>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,6 @@ import Gallery from "../components/Gallery";
 //components
 import Header from "../components/Header";
 import Music from "../components/Music";
-import Player from "../components/Player";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
