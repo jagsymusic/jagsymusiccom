8a20bb92d42bd6c3cbde010c882e672130f0cb25
Ida Merete Enholm <ida.enholm@me.com>
2021-01-09 18:49:58 +0100

    Menu (#4)
    * scroll function
    
    * nav outside header
    
    * fix small errors after merge

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 import myGallery from "../dataPictures";
 import Picture from "./Picture";
-import instagram from "../img/social/instagram.svg"
-
+import instagram from "../img/social/instagram.svg";
 
 const Gallery = () => {
-    const [pics, setPics] = useState(myGallery);
+  const [pics, setPics] = useState(myGallery);
 
-    return (
-        <div className="gallery-wrapper">
-            <div className="gallery container">
-                <h1 className="gallery-title">Gallery</h1>
-                <div className="gallery-container">
-                    {pics.map((img) => (
-                        <Picture img={img.img} name={img.name} key={img.id}/>
-                    ))}
-                </div>
-                <div className="follow-wrapper">
-                    <div className="follow-me-ig">
-                        <img src={instagram}  alt="instagram"/>
-                        <p>Follow me @jagsymusic for more</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="gallery-wrapper" id="gallery">
+      <div className="gallery container">
+        <h1 className="gallery-title">Gallery</h1>
+        <div className="gallery-container">
+          {pics.map((img) => (
+            <Picture img={img.img} name={img.name} key={img.id} />
+          ))}
         </div>
-    )
-}
+        <div className="follow-wrapper">
+          <div className="follow-me-ig">
+            <img src={instagram} alt="instagram" />
+            <p>Follow me @jagsymusic for more</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
